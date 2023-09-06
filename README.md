16b60cdef71498f3f3c46717c4fb3cb6dfa3b1ab
Daniel <U202121440@upc.edu.pe>
2023-09-05 20:53:36 -0500

    feat: logo-api & news-api

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # CatchUp
 
+## Summary
+CatchUp News project, illustrating fetching and presenting news from a news service, with news source logo obtained from a third-part service.
+## About the Framework 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.1.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {LogoApiService} from "./shared/services/logo-api.service";
+import {NewsApiService} from "./news/services/news-api.service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [LogoApiService,NewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
